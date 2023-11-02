18fbc95de924d86bc2a9fb4552dc639ea3a08d45
inthej <inthej12@gmail.com>
2023-11-02 21:21:39 +0900

    fix: signup 페이지 className 수정

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -5,43 +5,43 @@ import './Signup.css'
 const Signup = () => {
   return (
     <div className="signup-container">
-      <form action="/" className="form">
+      <form className="signup-form">
         <h1>Sign Up</h1>
         <p>Please fill in this form to create an account.</p>
-        <hr />
+        <hr className="signup-hr" />
 
-        <label htmlFor="email">
+        <label htmlFor="email" className="signup-label">
           <b>Email</b>
         </label>
-        <input type="text" placeholder="Enter Email" name="email" required />
+        <input type="text" placeholder="Enter Email" name="email" className="signup-input" required />
 
-        <label htmlFor="psw">
+        <label htmlFor="psw" className="signup-label">
           <b>Password</b>
         </label>
-        <input type="password" placeholder="Enter Password" name="psw" required />
+        <input type="password" placeholder="Enter Password" name="psw" className="signup-input" required />
 
-        <label htmlFor="psw-repeat">
+        <label htmlFor="psw-repeat" className="signup-label">
           <b>Repeat Password</b>
         </label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" required />
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" className="signup-input" required />
 
-        <label>
-          <input type="checkbox" checked name="remember" style={{ marginBottom: '15px' }} /> Remember me
+        <label className="signup-remember">
+          <input type="checkbox" checked name="remember" className="signup-checkbox" /> Remember me
         </label>
 
-        <p>
+        <p className="signup-terms">
           By creating an account you agree to our{' '}
-          <Link to="#" style={{ color: 'dodgerblue' }}>
+          <Link to="#" className="signup-terms-link">
             Terms & Privacy
           </Link>
           .
         </p>
 
-        <div className="clearfix">
-          <button type="button" className="cancelbtn">
+        <div className="signup-button-container">
+          <button type="button" className="signup-cancelbtn">
             Cancel
           </button>
-          <button type="submit" className="signupbtn">
+          <button type="submit" className="signup-signupbtn">
             Sign Up
           </button>
         </div>
